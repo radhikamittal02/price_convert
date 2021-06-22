16bc720ca8ce297ae7c06946ce26a5777f7ae3d3
Radhika <radhika@zapbuild.com>
2021-06-22 08:23:21 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Products from './Products';
+import Product from './Product';
 
 function App() {
   return (
     <div className="App">
-      <Products/>
+      <Product/>
     </div>
   );
 }
